1625d1bcce52c23425d7027018c2bcf6611e731d
Natalia Zaretska <nzaretska@gmail.com>
2024-07-17 22:43:42 +0200

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -28,3 +28,6 @@ function litres(time) {
 }
 
 //Opposites Attract https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
+function lovefunc(flower1, flower2){
+  return (flower1 + flower2) % 2 === 1
+}
