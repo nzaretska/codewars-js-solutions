3d46fdbd50adea933fa93f3c1687a9792bbd383b
Natalia Zaretska <nzaretska@gmail.com>
2024-07-17 22:25:44 +0200

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -1,10 +1,22 @@
+//Messi Goals https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
 function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   return laLigaGoals+copaDelReyGoals+championsLeagueGoals
 }
 
+//Make negative    https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
 function makeNegative(num) {
   if (num>0) {
     return 0-num
   } return num
 }
 
+//Game Move https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
+function move (position, roll) {
+  return position + roll * 2
+}
+
+//Personalized Message https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
+
+//Keep Hydrated https://www.codewars.com/kata/keep-hydrated-1/train/javascript
+
+//Opposites Attract https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
