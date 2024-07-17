748e0c6fee32d71da2b7b905b10ed0d2c760b315
Natalia Zaretska <nzaretska@gmail.com>
2024-07-17 22:17:06 +0200

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -1,3 +1,10 @@
 function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   return laLigaGoals+copaDelReyGoals+championsLeagueGoals
 }
+
+function makeNegative(num) {
+  if (num>0) {
+    return 0-num
+  } return num
+}
+
